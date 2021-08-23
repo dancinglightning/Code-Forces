d920190909951048f6ecddfa10e8c54dc3dc273e
Hanan Basheer <20b030018@iitb.ac.in>
2021-08-23 16:33:14 +0530

    Day 3

--- A. Taymyr is calling you/A.Taymyriscallingyou.cpp
+++ A. Taymyr is calling you/A.Taymyriscallingyou.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
 #include <string>
+#include <numeric>
 using namespace std;
 
 int main()
 {
-    
+    int a, b, z, ans, lcm;
+    cin>>a>>b>>z;
+    if(a>b)
+    {
+        lcm = a;
+    }
+    else
+    {
+        lcm = b;
+    }
+    while (1) 
+    {
+        if ( lcm%a==0 && lcm%b==0 ) 
+        {
+            break;
+        }
+        lcm++;
+    }
+    ans = z/lcm;
+    cout << ans;
     return 0;
 }
