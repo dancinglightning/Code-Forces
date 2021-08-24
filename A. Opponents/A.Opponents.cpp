cf7f91c7b7f96aa005368e59dda7de9cad6b6260
Hanan Basheer <20b030018@iitb.ac.in>
2021-08-24 18:05:10 +0530

    Day 4

--- A. Opponents/A.Opponents.cpp
+++ A. Opponents/A.Opponents.cpp
@@ -1,9 +1,42 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-int int main() 
+int main() 
 {
+    int n,d,max = 0,beats = 0;
+    cin>>n>>d;
+    for (size_t i = 0; i < d; i++)
+    {
+        string p;
+        bool beat = false;
+        cin>>p;
+        for (char j : p)
+        {
+            if (j=='0')
+            {
+                beat = true;
+            }
+        }
 
+        if (beat==true)
+        {
+            beats++;
+        }
+
+        else
+        {
+            beats = 0;
+        }
+
+        if (beats>max)
+            {
+                max = beats;
+            }
+
+    }
+
+    cout << max;
     
     return 0;
 }
