a3d80041716a3a69e1d8da9dda412bce4bac1837
Hanan Basheer <20b030018@iitb.ac.in>
2021-08-24 17:45:25 +0530

    Day 4

--- A. Wizards' Duel/A.Wizards'Duel.cpp
+++ A. Wizards' Duel/A.Wizards'Duel.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main()
 {
-
+    float l, h, e;
+    cin >> l >> h >> e;
+    float ans = (l*h)/(h+e);
+    cout << ans;
     return 0;
 }
