89cffbe643df77745288fc8503fe45a74cee34a1
Hanan Basheer <20b030018@iitb.ac.in>
2021-08-27 18:28:34 +0530

    Day 5

--- A. Free Ice Cream/A.FreeIceCream.cpp
+++ A. Free Ice Cream/A.FreeIceCream.cpp
@@ -1,8 +1,37 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-int int main() 
+int main() 
 {
+    long long int n, x, dis = 0;
+    cin >> n >> x;
+    for (size_t i = 0; i < n; i++)
+    {
+        string p;
+        long long int p1;
+        cin >> p >> p1;
+        if (p == "+")
+        {
+            x += p1;
+        }
+
+        else
+        {
+            if (p1 > x)
+            {
+                dis += 1;
+            }
+
+            else
+            {
+                x -= p1;
+            }
+        }
+
+    }
+    
+    cout << x << " " << dis;
     
     return 0;
 }
