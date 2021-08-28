9691113c8526c52430ab139327b5d76b3e4ec371
Hanan Basheer <20b030018@iitb.ac.in>
2021-08-28 21:23:53 +0530

    Day 6

--- A. Drazil and Date/A.DrazilandDate.cpp
+++ A. Drazil and Date/A.DrazilandDate.cpp
@@ -4,6 +4,15 @@ using namespace std;
 
 int main()
 {
-    
+    long long int a, b, n;
+    cin >> a >> b >> n;
+    if (abs(a)+abs(b) <= n && ((abs(a)+abs(b))-n)%2 == 0)
+    {
+        cout << "Yes";
+    }
+    else
+    {
+        cout << "No";
+    }
     return 0;
 }
